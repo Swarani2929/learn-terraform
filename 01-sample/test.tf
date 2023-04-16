45bb3dc768126bf972adaf526091c4a374886293
HP <swaranigupthapulluri@gmail.com>
2023-04-16 17:31:57 +0530

    updating ./learn-terraform

---
+++ 01-sample/test.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "ec2" {
+
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0daa2851fdf96b23e"]
+  tags = {
+    name = "test"
+  }
+}
