513d1c249b721432db61c0cfeb8696c248c94512
HP <swaranigupthapulluri@gmail.com>
2023-04-18 17:14:59 +0530

    updating ./learn-terraform

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -5,3 +5,13 @@ terraform {
     region = "us-east-1"
   }
 }
+
+resource "aws_instance" "ec2" {
+
+  ami = "ami-0089b8e98cd95257d"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = ["sg-0daa2851fdf96b23e"]
+  tags = {
+    Name = "test"
+  }
+}
