ffe08e1d9ec2b2c52d4c36a79e5a2c8c5680bcc0
HP <swaranigupthapulluri@gmail.com>
2023-04-21 17:02:36 +0530

    updating ./learn-terraform

--- roboshop-setup/ex-4/main.tf
+++ roboshop-setup/ex-4/main.tf
@@ -1,3 +1,18 @@
 module "ec2" {
+  for_each = var.instances
   source ="./ec2"
 }
+
+module "sg"
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
