0c923928193e8e58c56d42171fd9b82c040d94fd
HP <swaranigupthapulluri@gmail.com>
2023-05-21 10:16:01 +0530

    updating ./learn-terraform

--- prometheus-test-server/main.tf
+++ prometheus-test-server/main.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "node1" {
-  ami           = "ami-0089b8e98cd95257d"
+  ami           = "ami-02d09460c53924a65"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0daa2851fdf96b23e"]
   wait_for_fulfillment = true
@@ -10,7 +10,7 @@ resource "aws_spot_instance_request" "node1" {
 }
 
 resource "aws_spot_instance_request" "node2" {
-  ami           = "ami-0089b8e98cd95257d"
+  ami           = "ami-02d09460c53924a65"
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0daa2851fdf96b23e"]
   wait_for_fulfillment = true
