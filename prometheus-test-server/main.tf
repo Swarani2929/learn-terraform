aefbade7b4b06df69c6760f69f0c1493a57e5d31
HP <swaranigupthapulluri@gmail.com>
2023-05-21 10:21:22 +0530

    updating ./learn-terraform

--- prometheus-test-server/main.tf
+++ prometheus-test-server/main.tf
@@ -9,6 +9,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-02d09460c53924a65"
   instance_type = "t3.micro"
@@ -18,4 +24,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-node-server"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-node-server"
 }
